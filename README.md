936f6e1b062ebba6f6a4bc35ea9d9c0412631fc4
Rich Dougherty <rich@rd.nz>
2025-06-03 09:53:54 +1200

    Big performance boost: reinstrument on second call, not first

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Prototype of an extremely low overhead, simple to use Java dead code detector. `
 as an agent and automatically detects and prints out method usage.
 
 Methods are instrumented automatically, but *all instrumentation is automatically removed when no longer needed*,
-which means zero overhead after each method has been called once.
+which means zero overhead after each method has been called.[†](#footnotes)
 
 Additionally, effort is taken to make even the instrumented code path as fast as possible.
 
@@ -32,3 +32,9 @@ java -javaagent:nonop-<version>.jar MyApp
 ## Author
 
 [Rich Dougherty](https://rd.nz)
+
+## Footnotes
+
+† Note the current algorithm removes a method's instrumentation after the _second_ call to a method. Removing on the
+first call  is too  aggressive and results in too-frequent full class reinstrumentation vs the small overhead of running
+the method instrumentation twice.
--- src/main/java/nz/rd/nonop/internal/model/ClassUsageState.java
+++ src/main/java/nz/rd/nonop/internal/model/ClassUsageState.java
@@ -3,14 +3,17 @@
 package nz.rd.nonop.internal.model;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public final class ClassUsageState {
     // TODO: Optimize memory by omitting this field if can always be provided by caller
     private final WeakReference<? extends Class<?>> clazzWeakRef;
-    // TODO: Optimize memory usage by using a more space-efficient structure
-    private final Set<String> usedMethods = new HashSet<>();
+
+    // Track method call states: no entry = unused, CALLED_ONCE = first call, CALLED_MULTIPLE = second+ call
+    private final Map<String, MethodCallState> methodCallStates = new HashMap<>();
     private boolean reinstrumentationScheduled = false;
 
     public ClassUsageState(Class<?> clazz /*, Collection<String> methodSignatures */) {
@@ -21,11 +24,16 @@ public final class ClassUsageState {
         return clazzWeakRef;
     }
 
-    // TODO: Consider changing to avoid or delay reinstrumentation on first call (in case never needed again), but do immediately on second call
+    private enum MethodCallState {
+        CALLED_ONCE,
+        CALLED_MULTIPLE
+    }
+
     public enum MarkResult {
-        ALREADY_MARKED(false, false),
-        NEWLY_MARKED_INSTRUMENTATION_NEEDED(true, true),
-        NEWLY_MARKED_INSTRUMENTATION_ALREADY_SCHEDULED(true, false);
+        FIRST_CALL_NO_ACTION(true, false),
+        SECOND_CALL_INSTRUMENTATION_NEEDED(true, true),
+        SECOND_CALL_INSTRUMENTATION_ALREADY_SCHEDULED(true, false),
+        SUBSEQUENT_CALL_NO_ACTION(false, false);
 
         final boolean added;
         final boolean instrumentationNeeded;
@@ -44,26 +52,33 @@ public final class ClassUsageState {
         }
     }
 
-    // returns if needs reinstrumentation status newly set to true so reinstrumentation needs to be scheduled
-    // TODO: Consider an enum so return value is easier to understand
     public synchronized MarkResult recordMethodUsedAndDecideIfInstrumentationNeeded(String methodSignature) {
-        boolean added = usedMethods.add(methodSignature);
-        if (added) {
+        MethodCallState currentState = methodCallStates.get(methodSignature);
+
+        if (currentState == null) {
+            // First call - record it but don't trigger reinstrumentation yet
+            methodCallStates.put(methodSignature, MethodCallState.CALLED_ONCE);
+            return MarkResult.FIRST_CALL_NO_ACTION;
+        } else if (currentState == MethodCallState.CALLED_ONCE) {
+            // Second call - upgrade state and decide about reinstrumentation
+            methodCallStates.put(methodSignature, MethodCallState.CALLED_MULTIPLE);
+
             if (reinstrumentationScheduled) {
-                return MarkResult.NEWLY_MARKED_INSTRUMENTATION_ALREADY_SCHEDULED;
+                return MarkResult.SECOND_CALL_INSTRUMENTATION_ALREADY_SCHEDULED;
             } else {
                 reinstrumentationScheduled = true;
-                return MarkResult.NEWLY_MARKED_INSTRUMENTATION_NEEDED;
+                return MarkResult.SECOND_CALL_INSTRUMENTATION_NEEDED;
             }
         } else {
-            return MarkResult.ALREADY_MARKED;
+            // Third+ call - no action needed
+            return MarkResult.SUBSEQUENT_CALL_NO_ACTION;
         }
     }
 
     public synchronized Set<String> recordInstrumentationWithSnapshotOfUsage() {
-        Set<String> usageSnapshot = new HashSet<>(usedMethods);
+        // Create snapshot of all methods that have been called at least once
+        Set<String> usageSnapshot = new HashSet<>(methodCallStates.keySet());
         reinstrumentationScheduled = false;
         return usageSnapshot;
     }
-
 }
